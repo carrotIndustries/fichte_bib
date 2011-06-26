6eb9a09dfee09ee35c1d9fdaed753086bce9f06d
Lukas K <lu@0x83.eu>
2011-06-26 22:46:27 +0200

    defs.php fixed

--- defs.php
+++ defs.php
@@ -2,7 +2,7 @@
 	$dbsrv = "localhost";
 	$dbuser = "root";
 	$dbpasswd = "";
-	$dbname = "fichte_bib_init";
+	$dbname = "fichte_bib";
 	$quick_lend = "1337000000006";
 	$quick_return = "1337000000013";
 	$quick_cancel = "1337000000020";
